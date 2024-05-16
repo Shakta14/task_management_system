0d3870abe23490edfd7e1fa856decbe4a9995e0e
Shakta14 <madeshakta29@gmail.com>
2024-05-16 23:47:52 +0700

    some edit

--- task_list.php
+++ task_list.php
@@ -1,13 +1,13 @@
 <?php include 'db_connect.php' ?>
 <div class="col-lg-12">
 	<div class="card card-outline card-success">
-		<?php if ($_SESSION['login_type'] != 3) : ?>
+		<!-- <?php if ($_SESSION['login_type'] != 3) : ?>
 			<div class="card-header">
 				<div class="card-tools">
 					<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New project</a>
 				</div>
 			</div>
-		<?php endif; ?>
+		<?php endif; ?> -->
 		<div class="card-body">
 			<table class="table tabe-hover table-condensed" id="list">
 				<colgroup>
